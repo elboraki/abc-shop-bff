96a515846ade2a4d87d285f7b06bb6b45607e18f
elboraki <younes.elboraki@gmail.com>
2023-06-08 23:59:08 +0100

    connect to mongodb

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { router } from './config/routes';
+import mongoose from 'mongoose';
 
-
+mongoose.Promise=global.Promise
+mongoose.connect("mongodb://localhost/invoice-builder")
 const app = express();
 const PORT = 3000;
 app.use("/api",router)
