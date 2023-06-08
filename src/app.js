2bba0490515bcbf2a836931e3bb3e44f68580b76
elboraki <younes.elboraki@gmail.com>
2023-06-08 23:21:46 +0100

    add invoices and middlware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,20 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.use(function(req,res,next){
+ console.log("middlware");
+ next();
+})
+
+const invoices=[
+    {id:1,item:"Google X1",qte:4,date:new Date()},
+    {id:2,item:"Amazon Lambda",qte:3,date:new Date()},
+    {id:9,item:"Microsoft E1",qte:8,date:new Date()}
+]
+
+app.get("/invoices",(req,res)=>{
+    res.json(invoices)
+})
 app.get("/", (req, res) => {
     res.json({
         msg: "Welcome Younes How are you!"
