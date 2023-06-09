890e98627c28aec68339f1c4af886757578b440d
elboraki <younes.elboraki@gmail.com>
2023-06-10 00:35:42 +0100

    find and remove invoice functionality

--- src/api/controller/invoices.controller.js
+++ src/api/controller/invoices.controller.js
@@ -1,4 +1,4 @@
-import {StatusCodes} from "http-status-codes"
+import { StatusCodes } from "http-status-codes"
 import InvoiceModel from "../model/invoice.model";
 import Joi from "joi"
 const invoices = [
@@ -11,35 +11,59 @@ export default {
 
 
     findAll(req, res) {
-        InvoiceModel.find().then(invoices=>{
-            res.json(invoices).catch(error=>{
+        InvoiceModel.find().then(invoices => {
+            res.json(invoices).catch(error => {
                 res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
             })
         })
     },
 
-    create(req,res){
-        const {item,qte,date,due,rate,tax}=req.body;
-        const schema=Joi.object().keys({
-            item:Joi.string().required(),
-            qte:Joi.number().required(),
-            date:Joi.date().required(),
-            due:Joi.date().required(),
-            rate:Joi.number().optional(),
-            tax:Joi.number().optional(),
+    create(req, res) {
+        const { item, qte, date, due, rate, tax } = req.body;
+        const schema = Joi.object().keys({
+            item: Joi.string().required(),
+            qte: Joi.number().required(),
+            date: Joi.date().required(),
+            due: Joi.date().required(),
+            rate: Joi.number().optional(),
+            tax: Joi.number().optional(),
         })
 
-        const {error,value}=schema.validate(req.body);
+        const { error, value } = schema.validate(req.body);
 
-        if(error){
-           return  res.status(StatusCodes.BAD_REQUEST).json(error)
+        if (error) {
+            return res.status(StatusCodes.BAD_REQUEST).json(error)
         }
 
 
-        InvoiceModel.create(value).then(invoice=>{
-            res.status(StatusCodes.OK).json({invoice})
-        }).catch(error=>{
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:error})
+        InvoiceModel.create(value).then(invoice => {
+            res.status(StatusCodes.OK).json({ invoice })
+        }).catch(error => {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: error })
         })
+    },
+    findOne(req,res){
+        let {id} = req.params
+        InvoiceModel.findById(id).then(invoice=>{
+            if(!invoice){
+                return res.status(StatusCodes.NOT_FOUND).json({msg:"Invoice not found"})
+            }
+            return res.json(invoice)
+        }).catch(error=>{
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
+        })
+
+    },
+    delete(req,res){
+        let {id} = req.params
+        InvoiceModel.findByIdAndRemove(id).then(invoice=>{
+            if(!invoice){
+                return res.status(StatusCodes.NOT_FOUND).json({msg:"Invoice not found"})
+            }
+            return res.json(invoice)
+        }).catch(error=>{
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
+        })
+
     }
 }
--- src/config/routes.js
+++ src/config/routes.js
@@ -3,3 +3,5 @@ import InvoicesController from "../api/controller/invoices.controller"
 export const router =express.Router()
 router.get("/invoices",InvoicesController.findAll);
 router.post("/invoices",InvoicesController.create);
+router.get("/invoices/:id",InvoicesController.findOne)
+router.delete("/invoices/:id",InvoicesController.delete)
