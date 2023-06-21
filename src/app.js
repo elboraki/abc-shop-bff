edc91904a44ba5bc70c319962d206d88f9e5f9c6
elboraki <younes.elboraki@gmail.com>
2023-06-21 23:58:19 +0100

    fix cors issue bff

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ import { router } from './config/routes';
 import mongoose from 'mongoose';
 import logger from 'morgan'
 import swaggerUi from 'swagger-ui-express'
+import cors from "cors"
 import swaggerDocument from "./config/swagger.json"
-mongoose.Promise=global.Promise
+mongoose.Promise = global.Promise
 mongoose.connect("mongodb://localhost/invoice-builder")
 const app = express();
 const PORT = 3000;
@@ -13,26 +14,27 @@ const PORT = 3000;
 
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(cors())
 app.use(logger('combined'))
-app.use("/api-doc",swaggerUi.serve,swaggerUi.setup(swaggerDocument,{
-    explorer:true
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+    explorer: true
 }))
-app.use("/api",router)
+app.use("/api", router)
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
 
-    const error=new Error();
-    error.status=400;
-    error.message="Customer Error";
+    const error = new Error();
+    error.status = 400;
+    error.message = "Customer Error";
     next(error);
 
 
 })
-app.use((error,req,res,next)=>{
+app.use((error, req, res, next) => {
     res.status(error.status || 500)
     return res.json({
-        error:{
-            msg:error.message
+        error: {
+            msg: error.message
         }
     })
 })
